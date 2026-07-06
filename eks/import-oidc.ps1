b588d59a0fcd45a7b5955af898fd02fe90e82670
riverorepo <riveroaisolutions@gmail.com>
2026-07-06 17:06:29 -0400

    Stage 4-5: k8s manifests, eksctl config, arch diagram

--- eks/import-oidc.ps1
+++ eks/import-oidc.ps1
@@ -1,6 +1,7 @@
 # Adopt the account's existing GitHub OIDC provider into Terraform state,
 # then apply the rest (IAM role + policy). ECR repo is already created.
-$arn = "arn:aws:iam::241356815757:oidc-provider/token.actions.githubusercontent.com"
+$acct = (aws sts get-caller-identity --query Account --output text)
+$arn = "arn:aws:iam::${acct}:oidc-provider/token.actions.githubusercontent.com"
 
 terraform import aws_iam_openid_connect_provider.github $arn
 terraform plan
